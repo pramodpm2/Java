d982b435478849da56f17c171adf3ae5cd05e038
PRAMOD P M <53473387+pramodpm2@users.noreply.github.com>
2020-04-22 12:36:27 +0530

    Create ArrayIndexOutOfBoundsException.java

---
+++ ArrayIndexOutOfBoundsException.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        
+        try
+        {
+            int a[] = new int[6];
+            a[6]=9;
+        } 
+        catch(Exception e)
+        {
+            System.out.println("Stay In Your Limit......");
+        } 
+        
+    }
+}
